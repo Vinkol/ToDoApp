a5c9c94d0e9c24aaa2093773ae64587a66a2c4f0
Dmitriy Yashin <vinkoldima@gmail.com>
2025-02-14 01:42:36 +0300

    Initial commit

---
+++ ToDoList.Designer.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ToDoWinForms
+{
+    partial class ToDoList
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.BtnAdd = new System.Windows.Forms.Button();
+            this.BtnRemove = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // Подключение событий для перетаскивания
+            this.MouseDown += new MouseEventHandler(Form_MouseDown);
+            this.MouseUp += new MouseEventHandler(Form_MouseUp);
+            this.MouseMove += new MouseEventHandler(Form_MouseMove);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form_Closing);
+
+            // 
+            // listBox
+            // 
+            this.listBox.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Location = new System.Drawing.Point(12, 32);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(476, 173);
+            this.listBox.TabIndex = 0;
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(12, 211);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(363, 101);
+            this.textBox.TabIndex = 1;
+            // 
+            // BtnAdd
+            // 
+            this.BtnAdd.BackColor = System.Drawing.Color.Gray;
+            this.BtnAdd.Location = new System.Drawing.Point(381, 211);
+            this.BtnAdd.Name = "BtnAdd";
+            this.BtnAdd.Size = new System.Drawing.Size(107, 35);
+            this.BtnAdd.TabIndex = 2;
+            this.BtnAdd.Text = "Добавить";
+            this.BtnAdd.UseVisualStyleBackColor = false;
+            this.BtnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // BtnRemove
+            // 
+            this.BtnRemove.BackColor = System.Drawing.Color.Gray;
+            this.BtnRemove.Location = new System.Drawing.Point(381, 277);
+            this.BtnRemove.Name = "BtnRemove";
+            this.BtnRemove.Size = new System.Drawing.Size(107, 35);
+            this.BtnRemove.TabIndex = 3;
+            this.BtnRemove.Text = "Удалить";
+            this.BtnRemove.UseVisualStyleBackColor = false;
+            this.BtnRemove.Click += new System.EventHandler(this.Button2_Click_1);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 318);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 4;
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.BackColor = System.Drawing.Color.Red;
+            this.btn_exit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_exit.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_exit.Location = new System.Drawing.Point(465, 3);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(23, 23);
+            this.btn_exit.TabIndex = 5;
+            this.btn_exit.Text = "X";
+            this.btn_exit.UseVisualStyleBackColor = false;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // ToDoList
+            // 
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(500, 350);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.BtnRemove);
+            this.Controls.Add(this.BtnAdd);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.listBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ToDoList";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button BtnAdd;
+        private System.Windows.Forms.Button BtnRemove;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button btn_exit;
+    }
+}
+
---
+++ ToDoList.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ToDoWinForms
+{
+    public partial class ToDoList : System.Windows.Forms.Form
+    {
+        public ToDoList()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Button2_Click_1(object sender, EventArgs e)
+        {
+            // Проверяем, что в listBox есть выбранная задача
+            if (listBox.SelectedIndex >= 0)
+            {
+                // Удаляем выбранную задачу
+                listBox.Items.RemoveAt(listBox.SelectedIndex);
+            }
+            else
+            {
+                // Если задача не выбрана, показываем сообщение об ошибке
+                MessageBox.Show("Выберите задачу для удаления!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+            LoadTasksFromFile();  // Загружаем задачи при запуске
+        }
+
+        private void Form_Closing(object sender, FormClosingEventArgs e)
+        {
+            SaveTasksToFile();  // Сохраняем задачи перед закрытием
+        }
+
+        private void SaveTasksToFile()
+        {
+            using (StreamWriter writer = new StreamWriter("tasks.txt"))
+            {
+                foreach (var item in listBox.Items)
+                {
+                    writer.WriteLine(item);
+                }
+            }
+        }
+
+        private void LoadTasksFromFile()
+        {
+            if (File.Exists("tasks.txt"))
+            {
+                using (StreamReader reader = new StreamReader("tasks.txt"))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        listBox.Items.Add(line);
+                    }
+                }
+            }
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            // Проверяем, что в textBox есть текст
+            if (!string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                // Получаем текущий номер задачи, это просто количество элементов в listBox + 1
+                int taskNumber = listBox.Items.Count + 1;
+
+                // Создаем строку задачи с номером, описанием и датой
+                string task = $"{taskNumber}. {textBox.Text} - Срок: {dateTimePicker1.Value.ToShortDateString()}";
+
+                // Добавляем задачу в список
+                listBox.Items.Add(task);
+
+                // Очищаем текстовое поле после добавления
+                textBox.Clear();
+            }
+            else
+            {
+                // Если поле пустое, показываем сообщение об ошибке
+                MessageBox.Show("Введите описание задачи!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            // Закрывает форму
+    this.Close();
+        }
+
+        // Для хранения позиции мыши
+        private bool isMouseDown = false;
+        private int mouseX = 0;
+        private int mouseY = 0;
+
+        // Метод для начала перетаскивания
+        private void Form_MouseDown(object sender, MouseEventArgs e)
+        {
+            isMouseDown = true;
+            mouseX = e.X;
+            mouseY = e.Y;
+        }
+
+        // Метод для окончания перетаскивания
+        private void Form_MouseUp(object sender, MouseEventArgs e)
+        {
+            isMouseDown = false;
+        }
+
+        // Метод для перетаскивания окна
+        private void Form_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isMouseDown)
+            {
+                this.Left += e.X - mouseX;
+                this.Top += e.Y - mouseY;
+            }
+        }
+    }
+}
